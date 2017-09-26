28cd4cdd7760f953b5f3a90d2772e2e33fd012e7
00140051 <00140051@st-tep-lis2-n45.labst.academico.pucmm>
2017-09-25 21:06:25 -0400

    añadiendo meneos

--- src/primerprograma/PrimerPrograma.java
+++ src/primerprograma/PrimerPrograma.java
@@ -1,11 +1,19 @@
 package primerprograma;
 
+import java.util.Scanner; //importacion de codigo de la clase scanner desde la biblioteca java
+
 
 public class PrimerPrograma {
 
    
     public static void main(String[] args) {
        
+        System.out.println("Empezamos el programa \n");
+        String nombre;
+        int edad;
+        
+        Scanner entradaEscaner=new Scanner(System.in);
+        
     }
     
 }
